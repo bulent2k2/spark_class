36d6fdac4d899b42b807c51b8631045f0572d1a3
bulent2k2 <bulent2k2@gmail.com>
2020-07-19 21:18:22 -0400

    Add files via upload

--- TimeUsageSuite.scala
+++ TimeUsageSuite.scala
@@ -1,10 +1,44 @@
 package timeusage
 
 import org.apache.spark.sql.{ColumnName, DataFrame, Row}
-import org.junit.{Assert, Test}
-import org.junit.Assert.assertEquals
+import org.apache.spark.sql.types.{
+  DoubleType,
+  StringType,
+  StructField,
+  StructType
+}
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.{BeforeAndAfterAll, FunSuite}
 
 import scala.util.Random
 
-class TimeUsageSuite {
+@RunWith(classOf[JUnitRunner])
+class TimeUsageSuite extends FunSuite with BeforeAndAfterAll {
+
+  test("sample1") {
+    val pass = try {
+      Samples.sample1()
+      true
+    } catch { case _: Throwable => false }
+    assert(pass, "Didn't pass")
+  }
+
+  test("sample2") {
+    val pass = try {
+      Samples.sample2()
+      true
+    } catch { case _: Throwable => false }
+    assert(pass, "Didn't pass")
+  }
+
+  test("sample3") {
+    val pass = try { Samples.sample3(); true } catch { case _: Throwable => false }
+    assert(pass, "Didn't pass")
+  }
+
+  test("stop") {
+    val pass = try { Samples.stop(); true } catch { case _: Throwable => false }
+    assert(pass, "Didn't stop")
+  }
 }
